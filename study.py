e4162452de22815b17b97d816066ce67df9c6037
yany0703 <1804774189@qq.com>
2025-05-29 15:23:53 +0800

    nodetree

---
+++ study.py
@@ -0,0 +1,77 @@
+from pythonds.basic.stack import Stack
+
+
+
+# def count(s1,s2):
+#     l1 =list(s1)
+#     l2 = list(s2)
+#     l1.sort()
+#     l2.sort()
+#     if l1 == l2:
+#         return True
+#     else:
+#         return False
+# c1 = count("abcd","baedc")
+# print(c1)
+
+# def perchecker(string):
+#     s = Stack()
+#     a = True
+#     strnum = len(string)
+#     index_num = 0
+#     while index_num < strnum and a:
+#         if string[index_num] == "(":
+#             s.push(string[index_num])
+#         else:
+#             if s.isEmpty():
+#                 a = False
+#             else:
+#                 s.pop()
+#         index_num += 1
+#     if a and s.isEmpty():
+#         return True
+#     else:
+#         return False
+#
+# if __name__ == "__main__":
+#      print(perchecker("(())))))"))
+#      print(perchecker("((()))"))
+
+
+
+# def perchecker2(string):
+#     s = Stack()
+#     res = True
+#     num = len(string)
+#     start = 0
+#     while start < num and res != False:
+#         if string[start] in "({[":
+#             s.push(string[start])
+#         else:
+#             if s.isEmpty():
+#                 res = False
+#             else:
+#                 top = s.pop()
+#                 if not matche(top,string[start]):
+#                     res = False
+#         start += 1
+#     if res != False and s.isEmpty():
+#         return True
+#     else:
+#         return False
+# def matche(open,close):
+#     s1 = "({["
+#     s2 = ")}]"
+#     return s1.index(open) == s2.index(close)
+#
+#
+# if __name__ == "__main__":
+#     print(perchecker2("({[]})"))
+#     print(perchecker2("){}{){)}"))
+
+
+
+
+
+
+
